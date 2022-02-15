e0542e48a8b5b0cbf1273d0d0f22bc8b9f8d5ca0
Sampada <sampadahakke102@gmail.com>
2022-02-15 18:04:53 +0530

    [Sampada] ADD: Create Contacts

---
+++ AddressBook/AddressBook/Details.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Details
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailId { get; set; }
+
+
+
+        public void Display()
+        {
+            Console.WriteLine("First Name = " + this.FirstName);
+            Console.WriteLine("Last Name = " + this.LastName);
+            Console.WriteLine("Address = " + this.Address);
+            Console.WriteLine("City = " + this.City);
+            Console.WriteLine("State = " + this.State);
+            Console.WriteLine("Zip = " + this.Zip);
+            Console.WriteLine("Phone Number = " + this.PhoneNumber);
+            Console.WriteLine("Email Id = " + this.EmailId);
+
+        }
+
+
+    }
+
+    
+}
+    
---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========================================Welcome To Address Book==================================================");
+            Console.ReadLine();
+
+            //Creating Object Of Details Class
+            Details details = new Details();
+            //Assigning Variable
+            bool flag = true;
+
+            while (flag)
+            {
+                Console.WriteLine("\nEnter Number to Execute the Address book Program \n1. Create Details \n2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                //Using switch case to check options
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Contact Information : \nFirst Name, Last Name,Address,City,State, Zip, Phone Number and Email Id");
+                        details.FirstName = Console.ReadLine();
+                        details.LastName = Console.ReadLine();
+                        details.Address = Console.ReadLine();
+                        details.City = Console.ReadLine();
+                        details.State = Console.ReadLine();
+                        details.Zip = Console.ReadLine();
+                        details.PhoneNumber = Console.ReadLine();
+                        details.EmailId = Console.ReadLine();
+                        details.Display();
+                        break;
+
+                    case 2:
+                        flag = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+
+                }
+
+            } 
+        }
+    }
+}
+
